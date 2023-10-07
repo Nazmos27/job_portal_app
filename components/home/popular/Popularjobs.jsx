c7e5fce7775b5cf4cc892c8076a6d1ce7efda90b
Nazmos27 <nazmos789@gmail.com>
2023-10-08 02:05:08 +0600

    popular job section done,bug fixing in api integration

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,45 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
-import {useRouter} from 'expo-router';
+import { useRouter } from 'expo-router';
 import useFetch from '../../../hooks/useFetch'
 
-import  PopularJobCard from '../../common/cards/popular/PopularJobCard';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
 
 import styles from './popularjobs.style';
-import {COLORS,SIZES} from '../../../constants';
+import { COLORS, SIZES } from '../../../constants';
 
 
 const Popularjobs = () => {
-  const {isLoading,refetch,data,error} = useFetch('search',{query: 'react developer',num_of_pages : 1})
-  
+
+  const router = useRouter()
+  const {isLoading, refetch, data, error }  = useFetch(
+    'search',
+    { query: 'Python developer in Texas, USA',
+    page: '1',
+    num_pages: '1' }
+  )
+  // console.log("data check",data.length);
+
+  const [selectedJob,setSelectedJob] = useState()
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Popular jobs</Text>
         <TouchableOpacity>
-          <Text style={styles.headerBtn}>See all {data.length}</Text>
+          <Text style={styles.headerBtn}>See all</Text>
         </TouchableOpacity>
       </View>
 
       <View>
-        {isLoading? (
-          <ActivityIndicator size="large" color={COLORS.primary}/>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
         ) : error ? (
           <Text>Something went wrong -- {error}</Text>
         ) : (
           <FlatList
-            data={[1,2,3,4,5,6]}
-            renderItem={({item})=>(
+            data={data}
+            renderItem={({ item }) => (
               <PopularJobCard
-              item={item}
+                item={item}
+                handleCardPress={handleCardPress}
+                selectedJob={selectedJob}
               />
-            )} 
-            keyExtractor={item=>item?.job_id}
-            contentContainerStyle={{columnGap:SIZES.medium}}
+            )}
+            keyExtractor={item => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
-            />
+          />
         )}
-        
+
       </View>
 
     </View>
--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -1,25 +1,26 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
-import {RAPID_API_KEY} from 'react-native-dotenv'
+// import {RAPID_API_KEY} from 'react-native-dotenv'
 
 
 
 const useFetch = (endpoints,query) => {
     const [data,setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [error,setError] = useState(false);
+    const [error,setError] = useState(null);
+    
 
-    const rapidApiKey = RAPID_API_KEY
+    // const rapidApiKey = RAPID_API_KEY
 
     const options = {
         method: 'GET',
         url: `https://jsearch.p.rapidapi.com/${endpoints}`,
+        params: {...query},
         headers: {
-          'X-RapidAPI-Key': rapidApiKey,
+          'X-RapidAPI-Key': '6e9f6f23f3msh70c768fb8c19179p1d855ejsnb1be3c5bda5a',
           'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
         },
-        params: {...query},
       };
 
     const fetchData = async() => {
@@ -27,7 +28,8 @@ const useFetch = (endpoints,query) => {
 
         try{
             const response = await axios.request(options)
-            setData(response)
+            console.log(response.data.data.length);
+            setData(response.data.data)
             setIsLoading(false)
 
         }catch(error){
@@ -44,8 +46,7 @@ const useFetch = (endpoints,query) => {
         setIsLoading(true);
         fetchData();
     }
-
-    return {data,refetch,isLoading,error}
+return ({data,refetch,isLoading,error})
 }
 
 export default useFetch;
